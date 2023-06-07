77bd166aa1822cf0a8d00853310b70281d36df18
hayk98-hayrapetyan <hayk.hayrapetyan98@gmail.com>
2023-06-07 22:36:00 +0400

    init project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+const generateRandomId = () => crypto.randomUUID()
+
+const dropdowns = reactive([
+  { 
+    value: '', 
+    id: generateRandomId, 
+    options: [
+      { key: "Option 1", value: "Value 1" },
+      { key: "Option 2", value: "Value 2" },
+      { key: "Option 3", value: "Value 3" }
+    ] 
+  }
+])
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
